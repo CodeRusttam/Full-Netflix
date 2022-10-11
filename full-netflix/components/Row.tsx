cbb805f98b9bf9363bcea962e94f5dc05c37a1a2
Rusttam <rustem.eliyev.06@inbox.ru>
2022-10-11 05:29:27 -0700

    The Ui of home page done

--- full-netflix/components/Row.tsx
+++ full-netflix/components/Row.tsx
@@ -8,19 +8,16 @@ interface Props {
   movies: Movie[];
 }
 function Row({ title, movies }: Props) {
-  const rowRef = useRef<HTMLDivElement>(null);
-  const [isMoved, setIsMoved] = useState(false);
-  const handleClick = (direction: string) => {
-    setIsMoved(true);
-    if (rowRef.current) {
-      const { scrollLeft, clientWidth } = rowRef.current;
-      const scrollTo =
-        direction === "left"
-          ? scrollLeft - clientWidth
-          : scrollLeft + clientWidth;
-      rowRef.current.scrollTo({ left: scrollTo, behavior: "smooth" });
+    const rowRef = useRef<HTMLDivElement>(null)
+    const [isMoved, setIsMoved] = useState(false)
+    const handleClick = (direction: string) => {
+      setIsMoved(true)
+      if(rowRef.current){
+        const {scrollLeft, clientWidth} = rowRef.current
+        const scrollTo = direction === "left" ? scrollLeft - clientWidth : scrollLeft + clientWidth
+        rowRef.current.scrollTo({left: scrollTo, behavior: "smooth"})
+      }
     }
-  };
 
   return (
     <div className="h-40 space-y-0.5 md:space-y-2">
@@ -28,24 +25,13 @@ function Row({ title, movies }: Props) {
         {title}
       </h2>
       <div className="group relative md:-ml-2">
-        <ChevronLeftIcon
-          onClick={() => handleClick("left")}
-          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
-            !isMoved && "hidden"
-          }`}
-        />
-        <div
-          ref={rowRef}
-          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide  md:space-x-2.5 md:p-2"
-        >
-          {movies.map((movie) => (
-            <Thumbnail key={movie.id} movie={movie} />
-          ))}
-        </div>
-        <ChevronRightIcon
-          onClick={() => handleClick("right")}
-          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
-        />
+        <ChevronLeftIcon onClick={() => handleClick("left")} className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100${!isMoved && 'hidden' }`} />
+          <div ref={rowRef} className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide  md:space-x-2.5 md:p-2">
+            {movies.map((movie) => (
+               <Thumbnail key={movie.id} movie={movie} />
+            ))}
+          </div>
+        <ChevronRightIcon onClick={() => handleClick("right")} className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`} />
       </div>
     </div>
   );
